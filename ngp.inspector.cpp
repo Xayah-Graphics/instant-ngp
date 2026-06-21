b592c00969973fc1d8c9cd01f983cb6fa88ceef4
Xayah-Hina <ycxiang14sep@gmail.com>
2026-06-21 18:13:32 +0800

    feat: move sample_color_grid function declaration into the ngp::inspector::kernels namespace

--- ngp.inspector.cpp
+++ ngp.inspector.cpp
@@ -2,13 +2,13 @@ module;
 #include "ngp.train.h"
 #include <cstdint>
 
-namespace ngp::inspector::kernels {
-    void sample_color_grid(std::uint32_t dim_x, std::uint32_t dim_y, std::uint32_t dim_z, float reference_x, float reference_y, float reference_z, const std::uint16_t* params, float* sample_coords, std::uint16_t* density_input, std::uint16_t* rgb_input, std::uint16_t* network_output, float* output_rgb);
-}
-
 module ngp.inspector;
 import std;
 import ngp.train;
+
+namespace ngp::inspector::kernels {
+    void sample_color_grid(std::uint32_t dim_x, std::uint32_t dim_y, std::uint32_t dim_z, float reference_x, float reference_y, float reference_z, const std::uint16_t* params, float* sample_coords, std::uint16_t* density_input, std::uint16_t* rgb_input, std::uint16_t* network_output, float* output_rgb);
+}
 
 namespace ngp::inspector {
     Inspector::Inspector(const train::InstantNGP& trainer) : trainer{std::addressof(trainer)} {}
